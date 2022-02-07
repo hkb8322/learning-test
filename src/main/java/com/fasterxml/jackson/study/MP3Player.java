d6da7b1f5d851e4cebe44d1221c994b485ebbbab
hkb8322 <hkb8322@naver.com>
2022-02-08 02:06:52 +0900

    * Jackson 학습 테스트 - ObjectMapper.convertValue 테스트(1)

---
+++ src/main/java/com/fasterxml/jackson/study/MP3Player.java
@@ -0,0 +1,17 @@
+package com.fasterxml.jackson.study;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MP3Player {
+    private Integer volume;
+    private String modelNumber;
+}
