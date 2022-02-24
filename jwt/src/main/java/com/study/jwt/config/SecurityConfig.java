a46ab0ffad3e815ccc8cd812617bf2994420dbfc
hkb8322 <hkb8322@naver.com>
2022-02-24 23:24:53 +0900

    * JWT 학습 테스트 - Spring Security 설정

---
+++ jwt/src/main/java/com/study/jwt/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.study.jwt.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/*
+ 추가적인 설정을 위한 방법
+ 1. WebSecurityConfigurer 상속
+ 2. WebSecurityConfigurerAdapter 상속
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()                                // HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정
+                .antMatchers("/api/hello").permitAll()   // 해당 API에 대한 요청은 인증 없이 접근 허용
+                .anyRequest().authenticated();                      // 나머지 요청들은 모두 인증되어야 함
+    }
+}
