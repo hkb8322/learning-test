ce7c24d4e4289169877f7cfd2fe36e4332422400
hkb8322 <hkb8322@naver.com>
2022-02-25 01:09:16 +0900

    * JWT 학습 테스트 - 유효한 자격증명을 제공하지 않고 접근할 경우 401 오류를 리턴하기 위한 EntryPoint 추가   (AuthenticationEntryPoint 구현)

---
+++ jwt/src/main/java/com/study/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package com.study.jwt;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    /**
+     * 유효한 자격증명을 제공하지 않고 접근 시 401 오류 리턴
+     * @param request
+     * @param response
+     * @param authException
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
