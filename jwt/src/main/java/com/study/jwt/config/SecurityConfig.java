ed1da8453bfbde1c13034ec77852d4d0104a9e57
hkb8322 <hkb8322@naver.com>
2022-02-25 00:19:51 +0900

    * JWT 학습 테스트 - H2, 파비콘 관련 요청은 Spring Security 로직을 수행하지 않고 접근 가능하도록 처리

--- jwt/src/main/java/com/study/jwt/config/SecurityConfig.java
+++ jwt/src/main/java/com/study/jwt/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.study.jwt.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,6 +12,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                        , "/favicon.ico"
+                );
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
