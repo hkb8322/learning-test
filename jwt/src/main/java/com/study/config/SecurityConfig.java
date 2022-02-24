13650feb4b85071188885716c73ea020522fd323
hkb8322 <hkb8322@naver.com>
2022-02-25 00:49:06 +0900

    * JWT 학습 테스트 - 패키지 구조 리팩토링

--- jwt/src/main/java/com/study/config/SecurityConfig.java
+++ jwt/src/main/java/com/study/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.study.jwt.config;
+package com.study.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
