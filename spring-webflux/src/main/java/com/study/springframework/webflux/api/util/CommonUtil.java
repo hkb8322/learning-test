6abdcf789f44795a2d65d92740bf6742b073539d
hkb8322 <hkb8322@naver.com>
2022-03-29 00:32:03 +0900

    * Spring WebFlux 학습 테스트 - 공통 기능 추가(성공 시 리턴 형식, 요청 헤더 값 조회 관련)

--- spring-webflux/src/main/java/com/study/springframework/webflux/api/util/CommonUtil.java
+++ spring-webflux/src/main/java/com/study/springframework/webflux/api/util/CommonUtil.java
@@ -1,5 +1,6 @@
 package com.study.springframework.webflux.api.util;
 
+import org.reactivestreams.Publisher;
 import org.springframework.http.HttpCookie;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -7,6 +8,12 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.util.Optional;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
+import static org.springframework.web.reactive.function.server.ServerResponse.noContent;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 public class CommonUtil {
 
@@ -15,16 +22,27 @@ public class CommonUtil {
 
         for (String key : cookies.keySet()) {
             if (key.equals(cookieName)) {
-                return cookies.getFirst(key).getValue();
+                return cookies.getFirst(cookieName).getValue();
             }
         }
 
-        return null;
+        return "";
     }
 
-    public static Mono<ServerResponse> redirect(String location) {
+    public static <T, P extends Publisher<T>> Mono<ServerResponse> getDefaultServerResponse(P publisher, Class<T> elementClass) {
+        return ok()
+                .contentType(APPLICATION_JSON)
+                .body(fromPublisher(publisher, elementClass))
+                .switchIfEmpty(noContent().build());
+    }
+
+    public static Mono<ServerResponse> redirect(String requestUri) {
         return ServerResponse
-                .temporaryRedirect(URI.create(location))
+                .temporaryRedirect(URI.create(requestUri))
                 .build();
+    }
+
+    public static String getRequestHeaderValue(ServerRequest request, String headerName) {
+        return Optional.of(request.headers().header(headerName).get(0)).orElse("");
     }
 }
